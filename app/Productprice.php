ecd7f4092510aa71c5f3607881cf94ea37d67e1d
claudio <profeclaudiososa@yahoo.com.ar>
2019-03-05 09:02:59 -0300

    agrego table productprice

---
+++ app/Productprice.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Productprice extends Model
+{
+    public function product()
+    {
+      return $this->belongsTo(Product::class);
+    }
+
+    public function waist()
+    {
+      return $this->belongsTo(Waist::class);
+    }
+}
