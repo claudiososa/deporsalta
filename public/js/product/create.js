69bf6d8f861ac4376bd1ba8d91500a3124e21dad
claudio <profeclaudiososa@yahoo.com.ar>
2019-03-09 07:41:20 -0300

    avance en crear producto

--- public/js/product/create.js
+++ public/js/product/create.js
@@ -1,5 +1,16 @@
 
 $(document).ready(function () {  
+    $('#cost').change( function(){
+      $('[id ^=priceCost]').val('444')
+      
+      //alert('perdio el focus cost')
+    })
+
+    $('#client').change( function(){
+      $('[id ^=priceClient]').val('99')
+      //alert('perdio el focus client')
+    })  
+
     $('#priceReven').change( function() {        
     let diferencia =  $(this).val()-$('#priceCost').val()      
     let marginReseller = Math.round((diferencia / $('#priceCost').val())*100)     
--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -23,10 +23,29 @@
         @endif
       </div>
 
+      <div class="form-group @if($errors->has('brand_id')) has-danger @endif">
+        <label for="marginClient">Marca</label>
+        <select class="form-control" name="brand_id">
+          @foreach ($brands as $brand)
+            <option value="{{$brand->id}}">{{$brand->description}}</option>
+          @endforeach
+        </select>
+      </div>
+
+      <div class="form-group @if($errors->has('colour_id')) has-danger @endif">
+        <label for="marginClient">Color</label>
+        <select class="form-control" name="colour_id">
+          @foreach ($colours as $colour)
+            <option value="{{$colour->id}}">{{$colour->description}}</option>
+          @endforeach
+        </select>
+      </div>
+
+
       <div class="form-group @if($errors->has('priceCost')) has-danger @endif">
         {{-- <label for="description">Precio Costo Principal</label> --}}
-        <input type="text" name="cost" id="cost" value="" class="form-control" placeholder="Precio Costo General">
-        <input type="text" name="client" id="client" value="" class="form-control" placeholder="Precio Venta General">
+        <input type="text" name="cost" id="cost" value="" class="form-control" placeholder="Precio Costo">
+        <input type="text" name="client" id="client" value="" class="form-control" placeholder="Precio Venta">
         @if ($errors->has('priceCost{{$waist->id}}'))
           @foreach ($errors->get('priceCost') as $error)
             <div class="form-control-feedback">
@@ -52,42 +71,15 @@
         @endif
       </div>
         
-      @endforeach
+      @endforeach   
 
-      
-
-      <div class="form-group @if($errors->has('priceReven')) has-danger @endif">
-        {{-- <label for="description">Precio Revendedor</label> --}}
-        <input type="text" name="priceReven" id="priceReven" value="" class="form-control" 
-        placeholder="Precio para Revendedor">
-        @if ($errors->has('priceReven'))
-          @foreach ($errors->get('priceReven') as $error)
-            <div class="form-control-feedback">
-              {{$error}}
-            </div>
-          @endforeach
-        @endif
-      </div>
-
-      <div class="form-group @if($errors->has('priceClient')) has-danger @endif">
-        {{-- <label for="description">Precio Cliente Final</label> --}}
-        <input type="text" name="priceClient" id="priceClient" value="" class="form-control"
-        placeholder="Precio para Cliente Final">
-        @if ($errors->has('priceClient'))
-          @foreach ($errors->get('priceClient') as $error)
-            <div class="form-control-feedback">
-              {{$error}}
-            </div>
-          @endforeach
-        @endif
-      </div>
       
       <div class="form-group @if($errors->has('special')) has-danger @endif">
         <label for="special">Destacado</label> 
         <input type="checkbox" name="special" id="special" class="form-control">        
       </div>
 
-      <div class="form-group @if($errors->has('marginReseller')) has-danger @endif">
+      <!-- <div class="form-group @if($errors->has('marginReseller')) has-danger @endif">
         {{-- <label for="marginReseller">Margen Revendedor</label> --}}
         <input type="text" name="marginReseller" id="marginReseller" value="" class="form-control" readyonly>
         @if ($errors->has('marginReseller'))
@@ -109,28 +101,11 @@
             </div>
           @endforeach
         @endif
-      </div>
+      </div> -->
 
      
 
-      <div class="form-group @if($errors->has('brand_id')) has-danger @endif">
-        <label for="marginClient">Marca</label>
-        <select class="form-control" name="brand_id">
-          @foreach ($brands as $brand)
-            <option value="{{$brand->id}}">{{$brand->description}}</option>
-          @endforeach
-        </select>
-      </div>
-
-      <div class="form-group @if($errors->has('colour_id')) has-danger @endif">
-        <label for="marginClient">Color</label>
-        <select class="form-control" name="colour_id">
-          @foreach ($colours as $colour)
-            <option value="{{$colour->id}}">{{$colour->description}}</option>
-          @endforeach
-        </select>
-      </div>
-
+     
       <button type="submit" class="btn btn-success" name="button">Crear Producto</button>
     </form>
   </div>
