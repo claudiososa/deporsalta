eff737018d74f2e13c40bbd3d3e98e910d24dcb8
claudio <profeclaudiososa@yahoo.com.ar>
2019-03-12 16:19:19 -0300

    avance en sale

--- resources/views/brand/create.blade.php
+++ resources/views/brand/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-
+  <div class='container'>
   <div class="row">
     <form class="" action="/brand/create" method="post">
       {{csrf_field()}}
@@ -16,32 +16,10 @@
           @endforeach
         @endif
       </div>
-
-      <div class="form-group @if($errors->has('marginReseller')) has-danger @endif">
-        <label for="marginReseller">Margen Revendedor</label>
-        <input type="number" min="20" max="999" name="marginReseller" value="" class="form-control">
-        @if ($errors->has('marginReseller'))
-          @foreach ($errors->get('marginReseller') as $error)
-            <div class="form-control-feedback">
-              {{$error}}
-            </div>
-          @endforeach
-        @endif
-      </div>
-
-      <div class="form-group @if($errors->has('marginClient')) has-danger @endif">
-        <label for="marginClient">Margen Cliente Final</label>
-        <input type="number" min="20" max="999" name="marginClient" value="" class="form-control">
-        @if ($errors->has('marginClient'))
-          @foreach ($errors->get('marginClient') as $error)
-            <div class="form-control-feedback">
-              {{$error}}
-            </div>
-          @endforeach
-        @endif
-      </div>
+      
         <button type="submit" class="btn btn-success" name="button">Crear Marca</button>
 
     </form>
+  </div>
   </div>
 @endsection
