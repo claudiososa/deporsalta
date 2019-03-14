b8f26c42b2cfefd141c3e7316ad70a5ee3203bca
claudio <profeclaudiososa@yahoo.com.ar>
2019-03-14 18:15:01 -0300

    mejora en seleccionar item

--- resources/views/sale/create.blade.php
+++ resources/views/sale/create.blade.php
@@ -8,8 +8,8 @@
   <script src="{{asset('js/sale/getPrice.js')}}"></script>
   <div class="container">
   <div class="col-md-12">
-    <div class="col-md-12">
-      <h3>Venta de Producto :{{$product->description}}</h3>
+    <div class="col-md-12 alert alert-dark">
+      <h3>Producto seleccionado :{{$product->description}}</h3>
     </div>
     <div class="col-md-12">
 
@@ -19,6 +19,37 @@
         <input type="hidden" name="product_id" id="product_id" value="{{$product->id}}">
         <input type="hidden" name="sale_id" id="sale_id" value="{{$sale->id}}">
         <!-- <label for="description">Agregar Imagen 1</label> -->
+        <div class='row'>
+          <div class='col-md-5'>
+            <table class='table'>
+              <tr>
+                <td>Seleccionar</td>
+                <td>Talle</td>
+                <td>Stock</td>
+              </tr>
+              @forelse ($waists as $waist)
+              <tr>
+                <td><input class='from-control' type="radio" name="type" value="{{$waist->id}}"></td>
+                <td>{{$waist->description}}</td>
+                <td></td>
+                <td></td>
+              </tr>
+                @empty                
+                @endforelse   
+            </table>
+          </div>
+          <div class='col-md-4'>
+            <label for="priceUnit">Precio Unitario</label><br>
+            <input type="text" name="priceUnit" id="priceUnit" value="{{$product->priceClient}}" placeholder="precio unitario" >
+            <hr>
+            <label for="total">Total</label><br>
+            <input type="text" name="total" id="total" value="" readonly >
+            <hr>
+            <button type="submit" class="btn btn-success" name="button">Agregar Item</button>
+          </div>
+          <div class='col-md-3'>Foto</div>
+        </div>
+        
         <select class="form-control" name="waist_id" id="waist_id">
             <option value="0">Seleccione</option>
           @forelse ($waists as $waist)
@@ -29,12 +60,14 @@
         </select>
         <input max="20" min="1" type="number" name="quantity" id="quantity" value="1">
         <hr>
+        <label for="priceUnit">Precio Unitario</label><br>
         <input type="text" name="priceUnit" id="priceUnit" value="{{$product->priceClient}}" placeholder="precio unitario" >
         <hr>
         <hr>
+        <label for="total">Total</label><br>
         <input type="text" name="total" id="total" value="" readonly >
         <hr>
-        <button type="submit" class="btn btn-success" name="button">Confirmar Venta</button>
+        <button type="submit" class="btn btn-success" name="button">Agregar Item</button>
       </div>
     </form>
   </div>
